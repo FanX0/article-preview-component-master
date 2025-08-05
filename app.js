c373eda7b1cd81f1c702866831baa5929170188e
fanx <faridazhari46@gmail.com>
2025-08-05 18:28:32 +0700

    remove app.js file

--- app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("profileButton").addEventListener("click", function () {
-  const profile = document.getElementById("profile");
-  if (profile.style.display === "block") {
-    profile.style.display = "none";
-  } else {
-    profile.style.display = "block";
-  }
-});
